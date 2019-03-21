9183fba8e28fd630a27742a5c88a387c9f9f6bd1
Dave Chen, CW (ITS) <dave.cw.chen@hktdc.org>
2019-03-21 09:39:22 +0800

    message changes

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World students";
+        return "Hello Docker World PolyU students";
     }
 
     public static void main(String[] args) {
